fa0ea33e2a199112660b6c2e833a4e4351cd374c
sbellogig <sergio.bello@gig.com>
2025-10-07 17:03:15 +0200

    Adding test cases

--- TodoApi.Test/CustomWebApplicationFactory.cs
+++ TodoApi.Test/CustomWebApplicationFactory.cs
@@ -15,9 +15,18 @@ public class CustomWebApplicationFactory<TProgram>
     builder.ConfigureServices(services =>
     {
       var dbContextDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<TodoContext>));
-      if(dbContextDescriptor is not null) services.Remove(dbContextDescriptor);
+      if (dbContextDescriptor is not null) services.Remove(dbContextDescriptor);
 
       services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+      using var scope = services.BuildServiceProvider().CreateScope();
+      var context = scope.ServiceProvider.GetRequiredService<TodoContext>();
+
+      //Create initial data
+      context.TodoItems.Add(new TodoItem() { Id=1, Name="uno" });
+      context.TodoItems.Add(new TodoItem() { Id=2, Name="dos" });
+
+      context.SaveChanges();
     });
     
     builder.UseEnvironment("Development");
--- TodoApi.Test/TodoItemsControllerTests.cs
+++ TodoApi.Test/TodoItemsControllerTests.cs
@@ -1,5 +1,7 @@
+using System.Net.Http.Json;
 using ComponentTests;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using TodoApi.Models;
 
 namespace TodoApi.Test;
 
@@ -16,6 +18,27 @@ public class TodoItemsControllerTests : IClassFixture<CustomWebApplicationFactor
     public async void TestGetTodoItems()
     {
         var response = await _client.GetAsync("api/TodoItems");
+        Console.Write(response.Content);
+        response.EnsureSuccessStatusCode();
+        var data = await response.Content.ReadFromJsonAsync<TodoItem[]?>();
+        Console.Write(data);
+        Assert.NotNull(data);
+        for (int i = 0; i < data.Length; i++) {
+            var item = data[i]; 
+            Assert.Equal(i + 1, item.Id);
+        }
+    }
+
+    [Fact]
+    public async void TestGetTodoItem()
+    {
+        var response = await _client.GetAsync("api/TodoItems?id=1");
         Console.Write(response);
+        response.EnsureSuccessStatusCode();
+        var data = await response.Content.ReadFromJsonAsync<TodoItem?>();
+        Console.Write(data);
+        Assert.NotNull(data);
+        Assert.Equal(1, data.Id);
+        Assert.Equal("uno", data.Name);
     }
 }

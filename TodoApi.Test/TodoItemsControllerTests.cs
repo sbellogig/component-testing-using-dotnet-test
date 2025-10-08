ada091a5fd929cb5b6c51eb8f19a21fbbf4a240f
sbellogig <sergio.bello@gig.com>
2025-10-08 18:32:38 +0200

    Tests fixed

--- TodoApi.Test/TodoItemsControllerTests.cs
+++ TodoApi.Test/TodoItemsControllerTests.cs
@@ -1,30 +1,33 @@
 using System.Net.Http.Json;
+using System.Text.Json;
 using ComponentTests;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
 using TodoApi.Models;
+using Xunit.Abstractions;
 
 namespace TodoApi.Test;
 
 public class TodoItemsControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly HttpClient _client;
+    private readonly ITestOutputHelper _output;
 
-    public TodoItemsControllerTests(CustomWebApplicationFactory<Program> factory)
+    public TodoItemsControllerTests(CustomWebApplicationFactory<Program> factory, ITestOutputHelper output)
     {
         _client = factory.CreateClient();
+        _output = output;
     }
 
     [Fact]
     public async void TestGetTodoItems()
     {
         var response = await _client.GetAsync("api/TodoItems");
-        Console.Write(response.Content);
         response.EnsureSuccessStatusCode();
         var data = await response.Content.ReadFromJsonAsync<TodoItem[]?>();
-        Console.Write(data);
         Assert.NotNull(data);
         for (int i = 0; i < data.Length; i++) {
             var item = data[i]; 
+            _output.WriteLine(item.Name);
             Assert.Equal(i + 1, item.Id);
         }
     }
@@ -32,12 +35,13 @@ public class TodoItemsControllerTests : IClassFixture<CustomWebApplicationFactor
     [Fact]
     public async void TestGetTodoItem()
     {
-        var response = await _client.GetAsync("api/TodoItems?id=1");
-        Console.Write(response);
+        var response = await _client.GetAsync("api/TodoItems/1");
         response.EnsureSuccessStatusCode();
         var data = await response.Content.ReadFromJsonAsync<TodoItem?>();
-        Console.Write(data);
         Assert.NotNull(data);
+        var jsonOutput = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+        _output.WriteLine("Deserialized Data (as JSON):");
+        _output.WriteLine(jsonOutput);
         Assert.Equal(1, data.Id);
         Assert.Equal("uno", data.Name);
     }
